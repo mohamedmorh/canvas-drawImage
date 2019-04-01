c976195c073bcea987189ca2a0df24d98bd5c22d
medmorh <mc.marah.91@gmail.com>
2019-04-01 01:48:46 +0000

    two ways to check for loading images in canvas

---
+++ draw.js
@@ -0,0 +1,20 @@
+const c = document.querySelector('canvas').getContext('2d');
+console.log(c);
+c.canvas.width = window.innerWidth;
+c.canvas.height = window.innerHeight;
+
+let domImg = document.getElementsByTagName('img')[0]
+// because of reference of the image source
+// it's in the DOM so I must check the Window Object
+window.onload = function(){
+  c.drawImage(domImg,0,0)
+}
+
+
+let objImg = new Image();
+objImg.src = "save.jpeg";
+// because of the reference of the image source
+// its in the Object source so I must check the Object
+objImg.onload = function () {
+  c.drawImage(domImg,550,0)
+}
